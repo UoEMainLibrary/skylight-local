c4ebf56b088a9e1b1a8f9b8a2490d0e970687527
Ianthe Sutherland <ianthe88@gmail.com>
2022-10-11 19:59:47 +0100

    Updating copyright page

--- static/openbooks/licensing.php
+++ static/openbooks/licensing.php
@@ -2,7 +2,7 @@
     <div class="content byEditor">
         <h1>Licensing and Copyright Information</h1>
         <p>
-        All images within this collection are copyright the University of Edinburgh.
+        Most of the images within this collection are copyright the University of Edinburgh.
         </p>
         <p>
         If you are a rights holder and wish to request the removal of a work to which you hold the rights (or is not covered by a copyright exception under the UK Copyright, Designs and Patents Act 1988) please contact us in writing stating the following:
