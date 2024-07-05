0548de82275659c4c7ba4038b3c80286eee499d5
SchorchingPassion <litmantas@gmail.com>
2024-07-05 12:18:19 +0100

    LUC023A-233 Mimed accessibility fixes

--- theme/mimed/views/div_main_end.php
+++ theme/mimed/views/div_main_end.php
@@ -8,28 +8,28 @@
                 </div>
                 <div class="social-links">
                     <ul class="social-icons">
-                        <li><a href="https://www.facebook.com/pages/Edinburgh-University-Collection-of-Historic-Musical-Instruments-EUCHMI/144892895544842" class="facebook-icon" target="_blank" title="MIMEd on Facebook"></a></li>
-                        <li><a href="https://twitter.com/MIMEdinburgh" class="twitter-icon" target="_blank" title="MIMEd on Twitter"></a></li>
+                        <li><a href="https://www.facebook.com/pages/Edinburgh-University-Collection-of-Historic-Musical-Instruments-EUCHMI/144892895544842" class="facebook-icon" target="_blank" onclick="return warnNewTab()" title="MIMEd on Facebook"></a></li>
+                        <li><a href="https://twitter.com/MIMEdinburgh" class="twitter-icon" target="_blank" onclick="return warnNewTab()" title="MIMEd on Twitter"></a></li>
                     </ul>
                 </div>
             </div>
             <div class="footer-disclaimer">
                 <div class="footer-logo">
-                    <a href="https://www.ed.ac.uk/schools-departments/information-services/about/organisation/library-and-collections" target="_blank" class="luclogo" title="Library &amp; University Collections Home"></a>
+                    <a href="https://www.ed.ac.uk/schools-departments/information-services/about/organisation/library-and-collections" target="_blank" onclick="return warnNewTab()" class="luclogo" title="Library &amp; University Collections Home"></a>
                 </div>
                 <div class="footer-policies">
                     <p>This collection is part of <a href="<?php echo base_url(); ?>" title="University Collections Home">University Collections</a>.</p>
-                    <p><a href="https://www.ed.ac.uk/about/website/privacy" title="Privacy and Cookies Link"  target="_blank">Privacy &amp; Cookies</a>
-                        &nbsp;&nbsp;<a href="https://www.ed.ac.uk/information-services/library-museum-gallery/crc/services/copying-and-digitisation/image-licensing/takedown-policy" target="_blank" title="Takedown Policy Link">Takedown Policy</a>
+                    <p><a href="https://www.ed.ac.uk/about/website/privacy" title="Privacy and Cookies Link"  target="_blank" onclick="return warnNewTab()">Privacy &amp; Cookies</a>
+                        &nbsp;&nbsp;<a href="https://www.ed.ac.uk/information-services/library-museum-gallery/crc/services/copying-and-digitisation/image-licensing/takedown-policy" target="_blank" onclick="return warnNewTab()" title="Takedown Policy Link">Takedown Policy</a>
                         &nbsp;&nbsp;<a href="./licensing" title="Licensing and Copyright Link">Licensing &amp; Copyright</a>
-                        &nbsp;&nbsp;<a href="./accessibility" title="Website Accessibility Link" target="_blank">Accessibility</a></p>
-                    <p>Unless explicitly stated otherwise, all material is copyright &copy; <?php echo date("Y"); ?> <a href="https://www.ed.ac.uk" title="University of Edinburgh Home" target="_blank">University of Edinburgh</a>.</p>
+                        &nbsp;&nbsp;<a href="./accessibility" title="Website Accessibility Link" target="_blank" onclick="return warnNewTab()">Accessibility</a></p>
+                    <p>Unless explicitly stated otherwise, all material is copyright &copy; <?php echo date("Y"); ?> <a href="https://www.ed.ac.uk" title="University of Edinburgh Home" target="_blank" onclick="return warnNewTab()">University of Edinburgh</a>.</p>
                 </div>
                 <div class="is-logo">
-                    <a href="https://www.ed.ac.uk/information-services" target="_blank" class="islogo" title="University of Edinburgh Information Services Home"></a>
+                    <a href="https://www.ed.ac.uk/information-services" target="_blank" onclick="return warnNewTab()" class="islogo" title="University of Edinburgh Information Services Home"></a>
                 </div>
                 <div class="recognised-logo">
-                    <a href="https://www.museumsgalleriesscotland.org.uk/standards/recognition/" target="_blank" class="recognisedlogo" title="Recognised Collection of National Significance Link"></a>
+                    <a href="https://www.museumsgalleriesscotland.org.uk/standards/recognition/" target="_blank" onclick="return warnNewTab()" class="recognisedlogo" title="Recognised Collection of National Significance Link"></a>
                 </div>
             </div>
         </footer>
--- theme/mimed/views/header.php
+++ theme/mimed/views/header.php
@@ -3,15 +3,26 @@
 <!--[if lt IE 7 ]> <html class="no-js ie6" lang="en"> <![endif]-->
 <!--[if IE 7 ]>    <html class="no-js ie7" lang="en"> <![endif]-->
 <!--[if IE 8 ]>    <html class="no-js ie8" lang="en"> <![endif]-->
-<!--[if (gte IE 9)|!(IE)]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
+<html class="no-js" lang="en"> <!--<![endif]-->
+
 <head>
     <meta charset="utf-8">
 
-    <base href="<?php echo base_url() . index_page(); if (index_page() !== '') { echo '/'; } echo $this->config->item('skylight_url_prefix'); echo '/' ?>">
+    <base href="<?php echo base_url() . index_page();
+    if (index_page() !== '') {
+        echo '/';
+    }
+    echo $this->config->item('skylight_url_prefix');
+    echo '/' ?>">
 
     <title><?php echo $page_title; ?></title>
 
-    <link rel="pingback" href="<?php echo base_url() . index_page(); if (index_page() !== '') { echo '/'; } echo 'pingback'; ?>" />
+    <link rel="pingback" href="<?php echo base_url() . index_page();
+    if (index_page() !== '') {
+        echo '/';
+    }
+    echo 'pingback'; ?>" />
 
     <!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
     Remove this if you use the .htaccess -->
@@ -25,38 +36,49 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Place favicon.ico & apple-touch-icon.png in the root of your domain and delete these references -->
-    <link rel="shortcut icon" href="<?php echo base_url(); ?>theme/<?php echo $this->config->item('skylight_theme'); ?>/images/favicon.ico">
-    <link rel="apple-touch-icon" href="<?php echo base_url(); ?>theme/<?php echo $this->config->item('skylight_theme'); ?>/images/apple-touch-icon.png">
+    <link rel="shortcut icon"
+        href="<?php echo base_url(); ?>theme/<?php echo $this->config->item('skylight_theme'); ?>/images/favicon.ico">
+    <link rel="apple-touch-icon"
+        href="<?php echo base_url(); ?>theme/<?php echo $this->config->item('skylight_theme'); ?>/images/apple-touch-icon.png">
 
     <!-- CSS: implied media="all" -->
-    <link rel="stylesheet" href="<?php echo base_url(); ?>theme/<?php echo $this->config->item('skylight_theme'); ?>/css/style.css?v=2">
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/fancybox/source/jquery.fancybox.css?v=2.1.4" type="text/css" media="screen" />
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/fancybox/source/helpers/jquery.fancybox-buttons.css?v=1.0.5" type="text/css" media="screen" />
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/fancybox/source/helpers/jquery.fancybox-thumbs.css?v=1.0.7" type="text/css" media="screen" />
+    <link rel="stylesheet"
+        href="<?php echo base_url(); ?>theme/<?php echo $this->config->item('skylight_theme'); ?>/css/style.css?v=2">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/fancybox/source/jquery.fancybox.css?v=2.1.4"
+        type="text/css" media="screen" />
+    <link rel="stylesheet"
+        href="<?php echo base_url(); ?>assets/fancybox/source/helpers/jquery.fancybox-buttons.css?v=1.0.5"
+        type="text/css" media="screen" />
+    <link rel="stylesheet"
+        href="<?php echo base_url(); ?>assets/fancybox/source/helpers/jquery.fancybox-thumbs.css?v=1.0.7"
+        type="text/css" media="screen" />
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/flowplayer-7.0.4/skin/skin.css">
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/font-awesome/css/font-awesome.min.css">
-
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/bootstrap/css/bootstrap.min.css" type="text/css" media="screen">
 
     <!-- Uncomment if you are specifically targeting less enabled mobile browsers
     <link rel="stylesheet" media="handheld" href="css/handheld.css?v=2">  -->
 
     <!-- All JavaScript at the bottom, except for Modernizr which enables HTML5 elements & feature detects -->
-    <script src="<?php echo base_url()?>assets/modernizr/modernizr-1.7.min.js"></script>
-    <script src="<?php echo base_url()?>assets/jquery-1.11.0/jquery-1.11.0.min.js"></script>
-    <script src="<?php echo base_url()?>assets/jquery-ui-1.10.4/ui/minified/jquery-ui.min.js"></script>
-    <script src="<?php echo base_url()?>assets/bootstrap/js/bootstrap.min.js"></script>
-    <script src="<?php echo base_url()?>assets/jquery-1.11.0/jcarousel/jquery.jcarousel.min.js"></script>
-    <script src="<?php echo base_url()?>assets/google-analytics/analytics.js"></script>
+    <script src="<?php echo base_url() ?>assets/modernizr/modernizr-1.7.min.js"></script>
+    <script src="<?php echo base_url() ?>assets/jquery-1.11.0/jquery-1.11.0.min.js"></script>
+    <script src="<?php echo base_url() ?>assets/jquery-ui-1.10.4/ui/minified/jquery-ui.min.js"></script>
+    <script src="<?php echo base_url() ?>assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="<?php echo base_url() ?>assets/jquery-1.11.0/jcarousel/jquery.jcarousel.min.js"></script>
+    <script src="<?php echo base_url() ?>assets/google-analytics/analytics.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/leaflet.js"></script>
     <script src="https://cdn.rawgit.com/mejackreed/Leaflet-IIIF/master/leaflet-iiif.js"></script>
-    <script src="<?php echo base_url()?>assets/openseadragon/openseadragon.min.js"></script>
+    <script src="<?php echo base_url() ?>assets/openseadragon/openseadragon.min.js"></script>
     <!--<script type="text/javascript" src="//code.jquery.com/jquery-1.10.1.js"></script>-->
 
     <!-- Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo $ga_code ?>"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
         gtag('js', new Date());
 
         gtag('config', '<?php echo $ga_code ?>');
@@ -72,48 +94,61 @@
         };
     </script>
 
-    <?php if (isset($solr)) { ?><link rel="schema.DC" href="http://purl.org/dc/elements/1.1/" />
+    <?php if (isset($solr)) { ?>
+        <link rel="schema.DC" href="http://purl.org/dc/elements/1.1/" />
         <link rel="schema.DCTERMS" href="http://purl.org/dc/terms/" />
 
         <?php
 
-        foreach($metafields as $label => $element) {
+        foreach ($metafields as $label => $element) {
             $field = "";
-            if(isset($recorddisplay[$label])) {
+            if (isset($recorddisplay[$label])) {
                 $field = $recorddisplay[$label];
-                if(isset($solr[$field])) {
+                if (isset($solr[$field])) {
                     $values = $solr[$field];
-                    foreach($values as $value) {
-                        ?>  <meta name="<?php echo $element; ?>" content="<?php echo $value; ?>"> <?php
+                    foreach ($values as $value) {
+                        ?>
+                        <meta name="<?php echo $element; ?>" content="<?php echo $value; ?>"> <?php
                     }
                 }
             }
         }
-
     } ?>
 
 </head>
 
 <body>
 
-<div id="container">
+    <script>
+        function warnNewTab() {
+            return confirm("This link will open in a new tab. Proceed?");
+        }
+    </script>
 
+    <div id="container">
+        <div class="skip-links">
+            <a class="screen-reader-text" href="<?php echo current_url(); ?>#main">Skip to content</a>
+        </div>
         <header>
             <div id="collection-title">
-                <a href="https://www.ed.ac.uk" class="uoelogo" title="The University of Edinburgh Homepage Link" target="_blank"></a>
-                <a href="<?php echo base_url(); ?>mimed" class="mimedlogo" title="Musical Instrument Museums Edinburgh Home"></a>
-                <a href="http://www.stcecilias.ed.ac.uk/" class="menulogo" title="St Cecilia's Hall Link" target="_blank"></a>
+                <a href="https://www.ed.ac.uk" class="uoelogo" title="The University of Edinburgh Homepage Link"
+                    target="_blank" onclick="return warnNewTab()"></a>
+                <a href="<?php echo base_url(); ?>mimed" class="mimedlogo"
+                    title="Musical Instrument Museums Edinburgh Home"></a>
+                <a href="http://www.stcecilias.ed.ac.uk/" class="menulogo" title="St Cecilia's Hall Link"
+                    target="_blank" onclick="return warnNewTab()"></a>
             </div>
             <div id="collection-search">
                 <form action="./redirect/" method="post">
                     <fieldset class="search">
-                        <input type="text" name="q" value="<?php if (isset($searchbox_query)) echo urldecode($searchbox_query); ?>" id="q" />
+                        <input type="text" name="q"
+                            value="<?php if (isset($searchbox_query))
+                                echo urldecode($searchbox_query); ?>" id="q" />
                         <input type="submit" name="submit_search" class="btn" value="Search" id="submit_search" />
-                        <a href="./advanced" class="advanced">Advanced search</a>
+                        <a href="./advanced" class="advanced">Advanced<br>Search</a>
                     </fieldset>
                 </form>
             </div>
         </header>
 
         <div id="main" role="main" class="clearfix">
-
