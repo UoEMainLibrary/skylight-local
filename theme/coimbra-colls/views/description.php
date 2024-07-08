50916dc0c5cacb44e89669f0fb49ae98ece167ce
SchorchingPassion <litmantas@gmail.com>
2024-07-08 11:22:50 +0100

    LUC023A-233 Coimbra-Colls accessibility fixes made.

--- theme/coimbra-colls/views/description.php
+++ theme/coimbra-colls/views/description.php
@@ -21,7 +21,7 @@
 
                         $solr[$element][0] = "https://". $solr[$element][0];
                     }
-                      echo '<a title="URL Links for item" target="_blank" data-toggle="modal" data-target="#newTabNotice" data-href="' . $solr[$element][0] . '">' . $solr[$element][0] . '</a>';
+                      echo '<a title="URL Links for item" target="_blank" href="" data-toggle="modal" data-target="#newTabNotice" data-href="' . $solr[$element][0] . '">' . $solr[$element][0] . '</a>';
 
                 }
                 else {
