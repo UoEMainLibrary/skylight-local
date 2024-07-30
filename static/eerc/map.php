fe57dc54d6301325f6b9097ff0b18b8f1b3d0281
Mantas <litmantas@gmail.com>
2024-07-30 15:29:05 +0100

    LUC023A-233 EERC - Fixed a few links that opened in a new tab without warning and a few elements that did not focus highlight properly.

--- static/eerc/map.php
+++ static/eerc/map.php
@@ -513,6 +513,15 @@ map.add(clusters)
 */
   </script>
 </div>
+    <!-- Warn new tab for embeded links -->
+<script>
+  window.onload = function(){
+    const links = document.querySelectorAll('a[target="_blank"]');
+    links.forEach(link => {
+    link.setAttribute('onclick', 'return warnNewTab()');
+  })
+}
+</script>
 <br>
 <br>
     <div class="content byEditor">
