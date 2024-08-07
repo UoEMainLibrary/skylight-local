4fb5cad6e55c4a68c61a1f7ca1b6a86f973b8d73
SchorchingPassion <litmantas@gmail.com>
2024-08-07 16:20:14 +0100

    LUC023A-233 Guardbook accessibility fixes + broken link fixes.

--- theme/guardbook/views/header.php
+++ theme/guardbook/views/header.php
@@ -84,6 +84,15 @@
 </head>
 
     <body>
+        <div class="skip-links">
+            <a class="screen-reader-text" href="<?php echo current_url(); ?>#content">Skip to content</a>
+        </div>
+        <!-- New tab notice script -->
+        <script>
+            function warnNewTab() {
+              return confirm("This link will open in a new tab. Proceed?");
+            }
+        </script>
         <nav class="navbar navbar-default">
             <div class="container">
                 <div class="uoe-logo"></div>
@@ -125,4 +134,4 @@
             </div>
         </header>
 
-        <div class="container content">
+        <div id="content" class="container content">
