c6c1ba71dbc8a6f10ba2cffe431d421b95c4e726
Mantas <litmantas@gmail.com>
2024-08-13 14:08:44 +0100

    LUC023A-233 Art Collections website container and image fix & iog accessibility statement update & guardbook empty link deleted.

--- static/art/iiif.php
+++ static/art/iiif.php
@@ -1,8 +1,8 @@
-<div class="content">
+<div class="content container">
     <div class="content byEditor">
         <h1>International Image Interoperability Framework</h1>
 
-           <h3> <img src="https://upload.wikimedia.org/wikipedia/commons/e/e8/International_Image_Interoperability_Framework_logo.png" class ="iiifstatic" title="IIIF logo"></h3>
+           <h3> <img src="https://upload.wikimedia.org/wikipedia/commons/e/e8/International_Image_Interoperability_Framework_logo.png" class ="iiifstatic img-normal" title="IIIF logo"></h3>
         <p>
             The University of Edinburgh has adopted <a href="http://iiif.io" title="IIIF site" target ="_blank">IIIF</a>
             as our standard for image interoperability and workflow. This has a number of advantages for us, including:
@@ -14,7 +14,7 @@
                 <li>A culture of access to images through URLs, precluding the need to store images, thus saving storage space.</li>
             </ul>
         </p>
-            <h3><img src="https://images.is.ed.ac.uk/luna/images/LUNAIIIF80.png" title="LUNA IIIF logo" class=""></h3>
+            <h3><img src="https://images.is.ed.ac.uk/luna/images/LUNAIIIF80.png" title="LUNA IIIF logo" class="img-normal"></h3>
         <p>
             This collection makes use of IIIF. On the record page, you will notice we have embedded high resolution zooming, using the OpenSeadragon image viewer.
             This has been made possible by our high-resolution image platform, LUNA (<a href="http://images.is.ed.ac.uk">http://images.is.ed.ac.uk</a>), becoming IIIF-compliant.
