a37e54109c6c1a17ba416200295caa2271908b4b
Patryk Smacki <patryksmacki@Patryks-MacBook-Pro-93.local>
2025-10-08 12:50:19 +0100

    config repointed to archivesspace-live5

--- config/eerc.php
+++ config/eerc.php
@@ -9,27 +9,27 @@ $config['skylight_url_prefix'] = 'eerc';
 if(ENVIRONMENT == 'development') {
     if (strpos($_SERVER['HTTP_HOST'], "localhost") !== false) {
         $config['skylight_ga_code'] = 'G-RYHXTMGMHR';
-        $config['skylight_solrbase'] = 'http://lac-archivesspace-live4.is.ed.ac.uk:8983/';
-        $config['skylight_link_url'] = 'http://lac-archivesspace-live4.is.ed.ac.uk:8081';
+        $config['skylight_solrbase'] = 'http://lac-archivesspace-live5.is.ed.ac.uk:8983/';
+        $config['skylight_link_url'] = 'http://lac-archivesspace-live5.is.ed.ac.uk:8081';
     } else if (strpos($_SERVER['HTTP_HOST'], "test") !== false) {
         $config['skylight_ga_code'] = '';
-        $config['skylight_solrbase'] = 'http://lac-archivesspace-live4.is.ed.ac.uk:8983/';
-        $config['skylight_link_url'] = 'http://lac-archivesspace-live4.is.ed.ac.uk:8081';
+        $config['skylight_solrbase'] = 'http://lac-archivesspace-live5.is.ed.ac.uk:8983/';
+        $config['skylight_link_url'] = 'http://lac-archivesspace-live5.is.ed.ac.uk:8081';
         //$config['skylight_solrbase'] = 'http://lac-archivesspace-live4.is.ed.ac.uk:9123/';
         //$config['skylight_link_url'] = 'http://archives.collections.ed.ac.uk';
     }
 }
 else {
     $config['skylight_ga_code'] = 'G-L20JS09H7H';
-    $config['skylight_solrbase'] = 'http://lac-archivesspace-live4.is.ed.ac.uk:8983/';
-    $config['skylight_link_url'] = 'http://lac-archivesspace-live4.is.ed.ac.uk:8081';
+    $config['skylight_solrbase'] = 'http://lac-archivesspace-live5.is.ed.ac.uk:8983/';
+    $config['skylight_link_url'] = 'http://lac-archivesspace-live5.is.ed.ac.uk:8081';
     //$config['skylight_solrbase'] = 'http://lac-repo-live11.is.ed.ac.uk:8090/';
     //    $config['skylight_link_url'] = 'http://archives.collections.ed.ac.uk';
 }
 
 $config['skylight_archivesspace_user'] = 'apiread';
 $config['skylight_archivesspace_password'] = 'Auxilium1';
-$config['skylight_archivesspace_url'] = 'http://lac-archivesspace-live4.is.ed.ac.uk:8089';
+$config['skylight_archivesspace_url'] = 'http://lac-archivesspace-live5.is.ed.ac.uk:8089';
 $config['skylight_archivesspace_tree'] = '/repositories/15/resources/86984/tree';
 
 $config['skylight_repository_type'] = 'archivesspace'; // Demo 'dspace'
